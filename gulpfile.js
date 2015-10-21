827fcc88818e93269b268b559d594aca09fab016
Harry <harrygrumbar@hotmail.com>
2015-10-21 16:16:21 +0100

    use flexbox instead of jquery for centering

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['styles']);
 
 gulp.task('styles', function() {
     gulp.src('./assets/sass/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCss())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./assets/css/'));
 });
